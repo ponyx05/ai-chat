240b13e8ff67157de41d93f8c09d1ba6ceb769a1
Ronald-Sieber <chizha0226@163.com>
2026-05-02 11:27:45 +0800

    feat:jest测试完毕后关闭数据库连接，增加一个更新数据库表结构的prisma脚本

--- packages/backend/jest.config.ts
+++ packages/backend/jest.config.ts
@@ -6,7 +6,7 @@ const config: Config = {
   testMatch: ["**/*.test.ts"], //哪些是测试文件
   testTimeout: 10000, //允许测试的超时时间。当有网络请求时进行配置
   testEnvironment: "node", //测试环境
-  setupFiles: [], //测试前执行的文件
+  setupFilesAfterEnv: ["<rootDir>/src/__tests__/settings/setupTest.ts"], //测试前执行的文件
   moduleFileExtensions: ["ts", "js", "json"], //需要处理的文件扩展
   collectCoverageFrom: ["src/**/*.ts", "!src/**/*.d.ts"], //收集覆盖率的文件
   moduleNameMapper: {
