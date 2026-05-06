1d485b8d28f0d7ccf4247ba76a38d5368cdc733b
Ronald-Sieber <chizha0226@163.com>
2026-05-06 16:00:16 +0800

    fix:解决用户发送消息后，切换会话，消息跑到当前会话中的bug

--- apps/frontend/src/store/chat.ts
+++ apps/frontend/src/store/chat.ts
@@ -20,6 +20,9 @@ export const useChatStore = defineStore("chat", () => {
   const currentSession = computed(() =>
     sessions.value.find((s) => s.id === currentSessionId.value),
   );
+  const aiReplyingSession = computed(() =>
+    sessions.value.find((item) => item.id === aiReplyingSessionId.value),
+  );
 
   const fetchSessions = async () => {
     isLoading.value = true;
@@ -89,15 +92,15 @@ export const useChatStore = defineStore("chat", () => {
   };
 
   const sendMessage = async (content: string) => {
+    aiReplyingSessionId.value = currentSessionId.value;
     const userMessage: Message = {
       id: Date.now(),
       role: "user",
       content,
       createdAt: new Date().toISOString(),
     };
-    currentSession.value?.messages.push(userMessage);
+    aiReplyingSession.value?.messages.push(userMessage);
 
-    aiReplyingSessionId.value = currentSessionId.value;
     isAIThinking.value = true;
     // 回复加载状态依赖messages中最后一项且为ai回复的数据，因此添加占位数据，维持加载状态
     const dummyAIMessage: Message = {
@@ -106,22 +109,21 @@ export const useChatStore = defineStore("chat", () => {
       content: "",
       createdAt: new Date().toISOString(),
     };
-    currentSession.value?.messages.push(dummyAIMessage);
+    aiReplyingSession.value?.messages.push(dummyAIMessage);
 
-    let sessionId = currentSessionId.value;
     let fullContent = "";
 
     await new Promise<void>((resolve, reject) => {
       sendMessageSSE(
-        { sessionId: sessionId || undefined, content },
+        { sessionId: aiReplyingSessionId.value || undefined, content },
         {
           onChunk: (chunk) => {
             // console.log({ chunk });
             if (chunk.includes("</think>")) {
               isAIThinking.value = false;
               // 清除临时占位
-              currentSession.value?.messages.splice(
-                currentSession.value.messages.findIndex(
+              aiReplyingSession.value?.messages.splice(
+                aiReplyingSession.value.messages.findIndex(
                   (item) => item.id === -1,
                 ),
                 1,
@@ -131,15 +133,15 @@ export const useChatStore = defineStore("chat", () => {
             fullContent += chunk;
 
             const lastMsg =
-              currentSession.value?.messages[
-                currentSession.value.messages.length - 1
+              aiReplyingSession.value?.messages[
+                aiReplyingSession.value.messages.length - 1
               ];
             if (lastMsg?.role === "assistant") {
               // 拿到引用，前端分片拼接关键
               lastMsg.content = fullContent;
             } else {
               // dummyAIMessage清除后进入此分支，添加AI回复Message后上面才能拿引用
-              currentSession.value?.messages.push({
+              aiReplyingSession.value?.messages.push({
                 id: Date.now(),
                 role: "assistant",
                 content: fullContent,
@@ -148,17 +150,17 @@ export const useChatStore = defineStore("chat", () => {
             }
           },
           onSession: async (newSessionId) => {
-            sessionId = newSessionId;
+            currentSessionId.value = newSessionId;
             await fetchSessions();
             resolve();
           },
           onDone: async () => {
             isAIThinking.value = false;
-            aiReplyingSessionId.value = null;
             const currentIdx = sessions.value.findIndex(
-              (session) => session.id === sessionId,
+              (session) => session.id === aiReplyingSessionId.value,
             );
             if (currentIdx > 0) await fetchSessions(); //更新会话列表排序
+            aiReplyingSessionId.value = null;
             console.log({ currentSession: currentSession.value });
             resolve();
           },
@@ -169,11 +171,9 @@ export const useChatStore = defineStore("chat", () => {
       );
     });
 
-    if (!sessionId) {
+    if (!currentSessionId.value) {
       throw new Error("Session not created");
     }
-
-    currentSessionId.value = sessionId;
   };
 
   return {
